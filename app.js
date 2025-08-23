5b9f8edc27acdc7ac0052512a3909ebb0381b158
Salwa-mansour <alqadasisal@gmail.com>
2025-08-23 17:58:44 +0300

    add ejs issues found

--- app.js
+++ app.js
@@ -2,18 +2,24 @@
 const express = require("express")
 const app = express();
 const path = require("path");
+var ejs = require('ejs');
 var url = require('url');
+// app.js
+app.engine('ejs', require('ejs').__express);
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", ejs);
 
 app.get('/', (req, res) => {
     // Send the HTML file as the response
-    res.sendFile(path.join(__dirname,'views','index.html'));
+    // res.sendFile(path.join(__dirname,'views','index'));
+    res.render("index", { message: "EJS rocks!" });
 });
 
 app.get('/about', (req, res) => {
     // Send the HTML file as the response
     res.sendFile(path.join(__dirname,'views','about.html'));
 });
-app.get('/{*splat}', async (req, res) => {
+app.get('/{*}', async (req, res) => {
    // *splat matches any path without the root path. If you need to match the root path as well /, you can use /{*splat}, wrapping the wildcard in braces.
    res.sendFile(path.join(__dirname,'views','404.html'))
 })
