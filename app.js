2b9fda2f4b384304df27fad9c7c14f451ae2154e
Salwa-mansour <alqadasisal@gmail.com>
2025-08-26 10:31:16 +0300

    ejs added

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
 // express server
 const express = require("express")
 const app = express();
-const path = require("path");
+const path = require("node:path");
+var ejs = require('ejs');
 var url = require('url');
-
+// app.js
+//  app.engine('ejs', require('ejs').__express);
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 app.get('/', (req, res) => {
     // Send the HTML file as the response
-    res.sendFile(path.join(__dirname, 'index.html'));
+    // res.sendFile(path.join(__dirname,'views','index'));
+    res.render("index", { message: "EJS rocks!" });
 });
 
 app.get('/about', (req, res) => {
     // Send the HTML file as the response
-    res.sendFile(path.join(__dirname, 'about.html'));
+    // res.sendFile(path.join(__dirname,'views','about.html'));
+        res.render("about", { message: "EJS rocks!" });
 });
+app.use('/{*splat}', async (req, res) => {
+   // *splat matches any path without the root path. If you need to match the root path as well /, you can use /{*splat}, wrapping the wildcard in braces.
+   res.sendFile(path.join(__dirname,'views','404.html'))
+})
 //how to send error page
 const PORT = process.env.PORT || 3000;
 app.listen(PORT,()=>{
